1b735f7a6c1b8655305c84033991d751ed065a8d
alanabyan <alanabyan4@gmail.com>
2024-11-25 14:12:27 +0700

    feat: Card Division in Nevtik

--- app/page.tsx
+++ app/page.tsx
@@ -1,67 +1,11 @@
-import Image from "next/image";
-import { Poppins } from "next/font/google";
-import Link from "next/link";
-
-const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-})
+import CardDivision from "@/components/UI/Card";
 
 export default function Home() {
   return (
-    <div className="flex gap-16 justify-center bg-[#F8EDE3] h-screen items-center text-black">
-      {division.map((item, index) => (
-        <div
-          key={index}
-          className="w-full max-w-sm max-h-80 h-full bg-[#E5E1DA] border border-gray-200 rounded-lg shadow-[rgba(0,_0,_0,_0.2)_0px_60px_40px_-7px]"
-        >
-          <a href="#">
-            <Image
-              className="p-2 h-48 rounded-xl"
-              src={item.gambar}
-              alt="product image"
-              width={400}
-              height={100}
-            />
-          </a>
-          <div className="px-5 pb-5">
-            <a href="#">
-              <h5 className="text-lg font-semibold tracking-tight text-gray-900">
-                {item.divisi}
-              </h5>
-            </a>
-            <p className={`${poppins.className} text-sm font-light`}>
-              {item.deskripsi}
-            </p>
-            <div className="flex items-center justify-end mt-5">
-              <Link
-                href="#"
-                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-                Daftar Sekarang!
-              </Link>
-            </div>
-          </div>
-        </div>
-      ))}
+    <div className="flex justify-center bg-[#F8EDE3] h-screen items-center text-black">
+      <div className="grid grid-cols-3 gap-16">
+        <CardDivision />
+      </div>
     </div>
   );
 }
-
-const division = [
-  {
-    divisi: "ITNSA (IT Network System Administration)",
-    deskripsi: "ITNS is blablabla",
-    gambar: '/web.jpg'
-  },
-  {
-    divisi: "Cyber Security",
-    deskripsi: "",
-    gambar: '',
-  },
-  {
-    divisi: "Web Developer",
-    deskripsi: "",
-    gambar: '',
-  },
-];
---
+++ components/UI/Card.tsx
@@ -0,0 +1,72 @@
+import Image from "next/image";
+import Link from "next/link";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export default function CardDivision() {
+  return (
+    <>
+      {division.map((item, index) => (
+        <div
+          key={index}
+          className="w-full max-w-sm max-h-80 h-full bg-[#E5E1DA] border border-gray-200 rounded-lg shadow-[rgba(0,_0,_0,_0.2)_0px_60px_40px_-7px]"
+        >
+          <a href="#">
+            <Image
+              className="p-2 h-48 rounded-xl"
+              src={item.gambar}
+              alt="product image"
+              width={400}
+              height={100}
+            />
+          </a>
+          <div className="px-5 pb-5">
+            <a href="#">
+              <h5 className="text-lg font-semibold tracking-tight text-gray-900">
+                {item.divisi}
+              </h5>
+            </a>
+            <p className={`${poppins.className} text-sm font-light`}>
+              {item.deskripsi}
+            </p>
+            <div className="flex items-center justify-end mt-5">
+              <Link
+                href="#"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Daftar Sekarang!
+              </Link>
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+}
+
+const division = [
+  {
+    divisi: "ITNSA (IT Network System Administration)",
+    deskripsi: "ITNS is blablabla",
+    gambar: "/web.jpg",
+  },
+  {
+    divisi: "Cyber Security",
+    deskripsi: "",
+    gambar: "",
+  },
+  {
+    divisi: "Web Developer",
+    deskripsi: "",
+    gambar: "",
+  },
+  {
+    divisi: "Web Developer",
+    deskripsi: "",
+    gambar: "",
+  },
+];
